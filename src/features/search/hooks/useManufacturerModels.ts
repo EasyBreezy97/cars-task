a64772130e7745474b78d66aca2d3bbbd0470c83
Guram <gurikakhasia@gmail.com>
2024-03-15 15:00:07 +0400

    Fix enabled logic

--- src/features/search/hooks/useManufacturerModels.ts
+++ src/features/search/hooks/useManufacturerModels.ts
@@ -1,20 +1,14 @@
 import { QUERY_KEYS } from "@/common/utils/constants";
 import { useQuery } from "@tanstack/react-query";
 import { getManufacturerModelsList } from "../api/getManufacturerModelsList";
-import { useEffect } from "react";
 import { IManufacturerModelsResponse } from "../utils/types";
 
 function useManufacturerModelsList(manufacturerId: string) {
-  const { data, error, isLoading, refetch } =
-    useQuery<IManufacturerModelsResponse>({
-      queryKey: [QUERY_KEYS.ManufacturerModels],
-      queryFn: () => getManufacturerModelsList(manufacturerId),
-      enabled: false,
-    });
-
-  useEffect(() => {
-    refetch();
-  }, [manufacturerId, refetch]);
+  const { data, error, isLoading } = useQuery<IManufacturerModelsResponse>({
+    queryKey: [QUERY_KEYS.ManufacturerModels],
+    queryFn: () => getManufacturerModelsList(manufacturerId),
+    enabled: !!manufacturerId,
+  });
 
   return { models: data, error, isLoading };
 }
